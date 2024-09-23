65bdf6238c6e62ee41ba241807d30b78a192f597
James Kimani <mail4jameskimani@gmail.com>
2024-09-23 10:46:38 +0300

    mended merge conflict on configs file

--- _config.js
+++ _config.js
@@ -2,15 +2,9 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-<<<<<<< HEAD
    
     production: 'mongodb+srv://jameskimani2:4sy1NOu3iosx0IxS@devops-class-cluster.9kz3c.mongodb.net/?retryWrites=true&w=majority&appName=devops-class-cluster',
     development: 'mongodb+srv://jameskimani2:4sy1NOu3iosx0IxS@devops-class-cluster.9kz3c.mongodb.net/?retryWrites=true&w=majority&appName=devops-class-cluster',
     test: 'mongodb+srv://jameskimani2:4sy1NOu3iosx0IxS@devops-class-cluster.9kz3c.mongodb.net/?retryWrites=true&w=majority&appName=devops-class-cluster',
-=======
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
->>>>>>> test
 }
 module.exports = config;
